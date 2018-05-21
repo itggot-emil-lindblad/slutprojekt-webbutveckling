a618e7b63d48da82a6ba8673779d5b413c37772e
itggot-emil-lindblad <emil.lindblad@itggot.se>
2018-05-21 19:58:33 +0200

    Logo, menucontent and spritesheet

--- Slutprojekt/site/public/js/tabnavigation.js
+++ Slutprojekt/site/public/js/tabnavigation.js
@@ -1,7 +1,4 @@
 function tabnavigation(element){
-    // console.log(element)
-    // console.log(element.dataset.infopage)
-    // var element = document.querySelector(".navtab");
     var navtabs = document.querySelectorAll(".navtab")
     navtabs.forEach((nt ) => { nt.classList.remove("active") })
     
